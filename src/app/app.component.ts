c94b281ae3f0c22cd26928e9ae2ac9b8cf49e06f
Mike Miller <mike.miller@mikeslab.co>
2024-03-14 18:24:58 -0400

    Adding translations.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+
+import localeFr from '@angular/common/locales/fr';
+import localeEs from '@angular/common/locales/es';
+import localeDe from '@angular/common/locales/de';
+import localePl from '@angular/common/locales/pl';
+import localeEnGb from '@angular/common/locales/en-GB';
+import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
+
+registerLocaleData(localeFr, 'fr');
+registerLocaleData(localeEs, 'es');
+registerLocaleData(localeDe, 'de');
+registerLocaleData(localePl, 'pl');
+registerLocaleData(localeEnGb, 'en-GB');
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +23,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'numbers';
+
+  availableLanguages: string[] = [];
+  currentLanguage: string = 'en-US';
+
+  constructor(
+    @Inject(LOCALE_ID) public localeId: string,
+    public translateService: TranslateService,
+    private router: Router) {
+      this.availableLanguages = this.translateService.getLangs();
+      this.currentLanguage = this.translateService.currentLang;
+  }
+
+  onLanguageChange(language: string): void{
+    this.translateService.use(language);
+    this.localeId = language;
+    this.currentLanguage = language;
+
+    let currentUrl = this.router.url;
+    this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
+        this.router.navigate([currentUrl]);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,39 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: appInitializerFactory,
+      deps: [TranslateService],
+      multi: true,
+    },
+    { provide: LOCALE_ID, useValue: 'en-US' },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
+export function appInitializerFactory(translateService: TranslateService) {
+  return () => {
+    translateService.addLangs(['en-US', 'en-GB', 'fr', 'es', 'de', 'pl']);
+    translateService.setDefaultLang('en-US');
+
+    translateService.use(translateService.getBrowserCultureLang()!);
+  };
+}
