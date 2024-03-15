40b19a1b479294fe2656307db22f207c0704d3d2
Mike Miller <mgmiller@keller-na.com>
2024-03-15 15:47:57 -0400

    Beginning to build out the time input.

--- src/app/time-display/time-display.component.ts
+++ src/app/time-display/time-display.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class TimeDisplayComponent {
 
+  value1: number = -12345.54321;
+  value2: number = -12345.54321;
+  value3: number = -12345.54321;
+  value4: number = -12345.54321;
+  value5: number = -12345.54321;
+  value6: number = -12345.54321;
+  value7: number = -12345.54321;
+  value8: number = -12345.54321;
+
 }
--- src/app/time-input/time-input.component.ts
+++ src/app/time-input/time-input.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-time-input',
   templateUrl: './time-input.component.html',
   styleUrls: ['./time-input.component.scss']
 })
-export class TimeInputComponent {
+export class TimeInputComponent implements OnInit {
+  @Input() restorePreviousValue: boolean = true;
+  @Input() precision: number | undefined;
+  @Input() minValue: number | undefined;
+  @Input() maxValue: number | undefined;
+  @Input() hourCycle: boolean | undefined;
 
+  selectedLanguage: string = 'en-US';
+
+  @Input() valueAsNumber: number = 0;
+  @Output() valueAsNumberChange: EventEmitter<number> = new EventEmitter<number>;
+
+  valueAsString: string = '16:00';
+
+  previousValueAsNumber: number = 0;
+
+  tempValue: string = '';
+  tempValueAsDate: Date | undefined | null;
+
+  constructor(private translateService: TranslateService) {}
+
+  ngOnInit(): void {
+    this.translateService.onLangChange.subscribe({
+      next: ((value: LangChangeEvent) => {
+        this.selectedLanguage = value.lang;
+
+        if (this.hourCycle == undefined) {
+          let locale = new Intl.DateTimeFormat(this.selectedLanguage, {hour: 'numeric'}).resolvedOptions();
+          // let hourCycles = locale.hourCycles;
+          console.log('hourCycle', locale)
+          // this.hourCycle = hourCycle;
+        }
+      })
+    });
+  }
+
+  onFocus(event: FocusEvent) {
+    console.log('onFocus', event);
+    if (this.restorePreviousValue) {
+      this.previousValueAsNumber = this.valueAsNumber;
+      this.valueAsString = '';
+    }
+  }
+
+  onBlur(event: FocusEvent) {
+    console.log('onBlur', event);
+
+    if (event.target != null) {
+      let target = event.target as HTMLInputElement;
+
+      let currentValue = target.value;
+
+      this.tempValue = currentValue;
+      this.tempValueAsDate = target.valueAsDate;
+    }
+  }
+
+  onKeypress(event: KeyboardEvent) {
+    console.log('onKeypress', event);
+  }
 }
